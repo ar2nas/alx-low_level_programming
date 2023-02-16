376f10e6922cb554b47bb19f40d5352dacdc754d
Your Name <you@example.com>
2023-02-16 14:41:34 -0800

    4 and 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,9 @@
  *
  * description - 'programing is like building with puts'
  *
- * return : 0 (succes)
+ * Return: 0
  */
-main(void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * main - print with printf
- * return - 0 (success)
+ * main -print with printf
+ *
+ * Return: 0
  */
-main(void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
